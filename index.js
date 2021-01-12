6fe556f5164296e44b77cf0eaa779924fde3385c
Edson Carlos de Melo Ferreira <edsonka@kalunga.com.br>
2021-01-12 17:34:28 -0300

    alteração para chamada por app

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const http = require('http');
 const fs = require('fs').promises;
 const host = 'localhost';
-const port = 8001;
 const sql = require('mssql');
+const connStr1 = "Server='DESKTOP-TKT2S2J\\NEWSQL'; Database=Db; User Id=sa; Password=321654987;";
 const connStr = "Server=kasrvdb01; Database=DB_Site; User Id=sa; Password=K@lunga;";
-//const connStr = "Server='DESKTOP-TKT2S2J\\NEWSQL'; Database=Db; User Id=sa; Password=321654987;";
+
+const express = require('express')
+//const expressOasGenerator = require('express-oas-generator');
+const app = express()
+const port = 3000
+//expressOasGenerator.getSpec().parameters;
+
+//expressOasGenerator.init(app,{} ); 
+
+// Load the full build.
+
+// var _ = require('lodash');npom
+// expressOasGenerator.init(app, function(spec) {
+//     _.set(spec, 'info.title', 'New Title');
+//     _.set(spec, 'paths[\'/path\'].get.parameters[0].example', 2);
+//     return spec;
+// });
+
+// expressOasGenerator.init(
+//     app,
+//     function(spec) { return spec; },
+//     'path/to/a/file/filename.json',
+//     60 * 1000,
+//     'api-docs',
+//     ['User', 'Student'],
+//     ['users', 'students'],
+//     ['production'],
+//     true
+//   )
+
 
 //fazendo a conexão global
 sql.connect(connStr)
@@ -20,77 +49,51 @@ function execSQLQuery(sqlQry, res){
                  res.end(JSON.stringify(fetch));
                })
                .catch(err => res.end(err));
-  }
-
-const requestListener = function (req, res) {
-    switch (req.url) {
-        
-        case "/":
-            fs.readFile(__dirname + "/index.html", {"flag": 'rs'})
-            .then(contents => {
-                res.setHeader("Content-Type", "text/html");
-                res.writeHead(200);
-                res.end(contents);
-            })
-            break
-
-        case "/index":
-            fs.readFile(__dirname + "/index.html", {"flag": 'rs'})
-            .then(contents => {
-                res.setHeader("Content-Type", "text/html");
-                res.writeHead(200);
-                res.end(contents);
-            })
-            break
-
-        case "/produtos":
-            execSQLQuery('SELECT top 5 * from sys.tables', res);
-            break
-
-        case "/consulta":
-            fs.readFile(__dirname + "/consultas.html")
-            .then(contents => {
-                res.setHeader("Content-Type", "text/html");
-                res.writeHead(200);
-                res.end(contents);
-            })
-            break
-
-            case "/validalogin":
-
-
-                 var username = request.body.username;
-                 var password = request.body.password;
-                var username = '1';
-                var password = '00';
-                if (username && password) {
-                    
-                    execSQLQuery('SELECT * FROM TB_ClienteLoginSite WHERE cd_LoginSite   = ? AND CD_DC = ?', [username, password], function(error, res, fields) {
-                        if (res.length > 0) {
-                            request.session.loggedin = true;
-                            request.session.username = username;
-                            response.redirect('/home');
-                        } else {
-                            response.send('Incorrect Username and/or Password!');
-                        }
-                        response.end();
-                    });
-                } else {
-                    response.send('Please enter Username and Password!');
-                    response.end();
-                }
-    
-                break
-
-            case "/apiproduto":
-                execSQLQuery('SELECT top 10 cd_produto, nm_produto FROM TB_PRODUTO', res);
-                break
-
-    }
 }
-const server = http.createServer(requestListener);
 
-server.listen(port, host, () => {
-    console.log(`Servidor de Api'S rodando em http://${host}:${port}`);
-});
 
+app.get('/ariel', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+app.get('/consulta', (req, res) => {
+    fs.readFile(__dirname + "/consultas.html")
+    .then(contents => {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(contents);
+    })
+})
+
+
+app.get('/', (req, res) => {
+    fs.readFile(__dirname + "/index.html", {"flag": 'rs'})
+    .then(contents => {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(contents);
+    })
+})
+
+
+app.get('/produtos', (req, res) => {
+    execSQLQuery('SELECT top 20 cd_produto, nm_produto, qtd_estoque, vl_precovenda FROM TB_PRODUTO', res);
+})
+
+
+app.get('/apiproduto', (req, res) => {
+    execSQLQuery('SELECT top 10 cd_produto, nm_produto, qtd_estoque, vl_precovenda FROM TB_PRODUTO', res);
+})
+
+
+app.get('/teste', (req, res) => {
+    fs.readFile(__dirname + "/teste.html", function(err, data){
+        res.end(data);
+    })
+})
